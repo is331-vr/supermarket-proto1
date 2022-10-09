f95d51fde7257a8d1ed11dba17de6a3388f51d15
is331-vr <is331.vr@gmail.com>
2022-10-09 17:45:28 +0800

    9 Oct Calvin
    Added avatar, and dummy text box

--- Supermarket_v2/Assets/NPC.cs
+++ Supermarket_v2/Assets/NPC.cs
@@ -1,38 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NPC : MonoBehaviour
 {
     private GameObject triggeringNpc;
     private bool triggering;
+    
+   
+    List<InputDevice> leftHandedControllers = new List<InputDevice>();
+    InputDevice device;
+    bool deviceSet = false;
 
     public GameObject npcText;
 
     [SerializeField] private Animator myAnimationController;
+    [SerializeField] XRController controller;
 
     //public Transform target;
     private GameObject player;
-  
+
+
 
     void Start()
     {
-        player = GameObject.Find("Player");
+
+        player = GameObject.Find("XR Origin");
+        //Debug.Log(player);
     }
 
 
     void Update()
     {
+        if (leftHandedControllers.Count<1)
+        {
+            var desiredCharacteristics = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+            InputDevices.GetDevicesWithCharacteristics(desiredCharacteristics, leftHandedControllers);
+            
+            
+
+        }
+
+        if(leftHandedControllers.Count == 1 && !deviceSet)
+        {
+            device = leftHandedControllers[0];
+            //Debug.Log(device.name + device.characteristics);
+            deviceSet = true;
+        }
+
         if (triggering)
         {
             npcText.SetActive(true);
-
-            if (Input.GetKeyDown(KeyCode.E))
+            bool triggerValue;
+            if (device.TryGetFeatureValue(CommonUsages.triggerButton, out triggerValue) && triggerValue)
             {
+                //Debug.Log("Trigger activated");
                 //print("Congratulations! Jo Mama");
                 npcText.SetActive(false);
                 myAnimationController.SetBool("PlayTalk", true);
-
+                //Debug.Log(player);
                 transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
 
 
@@ -66,4 +94,6 @@ public class NPC : MonoBehaviour
             triggeringNpc = null;
         }
     }
+
+
 }
